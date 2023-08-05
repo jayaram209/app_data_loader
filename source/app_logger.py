cf54e9ecdfaf6645aff4dc4f27b9d59e69d3366a
jayaram209 <user.email=jairamc188@gmail.com>
2023-08-05 20:50:11 +0530

    fix linting and black

--- source/app_logger.py
+++ source/app_logger.py
@@ -2,10 +2,10 @@ import logging
 
 logging.basicConfig(
     format="%(asctime)s %(message)s",
-    filename='/home/jayaram/app_data_loader/example.log',
-    encoding='utf-8',
-    filemode='w'
+    filename="/home/jayaram/app_data_loader/example.log",
+    encoding="utf-8",
+    filemode="w",
     level=logging.DEBUG,
-    datefmt='%m/%d/%Y %I:%M:%S %p'
+    datefmt="%m/%d/%Y %I:%M:%S %p",
 )
 logger = logging.getLogger(__name__)
--- source/main.py
+++ source/main.py
@@ -1,14 +1,14 @@
-import datetime
-import os
 from app_logger import logger
 
-def check_logging():
-    logger.debug('debug message')
-    logger.info('info message')
-    logger.warning('warn message')
-    logger.error('error message')
-    logger.critical('critical message')
 
-if __name__== "__main__":
+def check_logging():
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warn message")
+    logger.error("error message")
+    logger.critical("critical message")
+
+
+if __name__ == "__main__":
     check_logging()
     print("Process finsihed")
