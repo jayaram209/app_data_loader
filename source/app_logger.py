9d15e2f3e773ba28397bf7a886d1daa075d3b12c
jayaram209 <user.email=jairamc188@gmail.com>
2023-08-05 20:14:34 +0530

    basic setup and directory structure created

---
+++ source/app_logger.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    filename='/home/jayaram/app_data_loader/example.log',
+    encoding='utf-8',
+    filemode='w'
+    level=logging.DEBUG,
+    datefmt='%m/%d/%Y %I:%M:%S %p'
+)
+logger = logging.getLogger(__name__)
